9271ce9e67ec2794b32427841759b489d3e05765
jasonaibrahim <jasonaibrahim@gmail.com>
2013-09-30 19:53:26 -0700

    Everything seems to be working. One questionable line is commented out. Still not sure why it works....

--- rip_router.py
+++ rip_router.py
@@ -9,7 +9,6 @@ class RIPRouter (Entity):
 		def init_helper():
 			self.packet_actions[RoutingUpdate] = self.update
 			self.packet_actions[DiscoveryPacket] = self.discover
-			self.packet_actions[Packet] = self.forward
 		self.packet_actions = {}
 		self.routing_table = RoutingTable(self)
 		init_helper()
@@ -18,7 +17,10 @@ class RIPRouter (Entity):
 		try:
 			self.packet_actions[type(packet)](packet, port)
 		except KeyError:
-			return 
+			if isinstance(packet, Packet):
+				self.forward(packet, port)
+			else:
+				print("Unknown packet.")
 
 	def hand_off(self, dest):
 		"""Return the minimum cost port to get to destination dest"""
@@ -28,14 +30,15 @@ class RIPRouter (Entity):
 		for neighbor in self.routing_table.neighbors:
 			update = RoutingUpdate()
 			for dest in self.routing_table.destinations:
-				if self.table()[dest].index(self.routing_table.minimum(self.table()[dest])) == self.routing_table.neighbors[neighbor]:
-					min_cost = float('inf')
-				else:
-					min_cost = self.routing_table.minimum(self.table()[dest])
+				# if self.table()[dest].index(self.routing_table.minimum(self.table()[dest])) == self.routing_table.neighbors[neighbor]:
+				# if neighbor in self.table()[dest]:
+				# 	print("WOWOW")
+				# 	min_cost = float('inf')
+				# else:
+				min_cost = self.routing_table.minimum(self.table()[dest])
 				if dest != neighbor and min_cost != float('inf'):
 					update.add_destination(dest, min_cost)
 			if update.paths:
-				print(neighbor, update.paths)
 				self.send_packet(update, neighbor)
 
 	def send_packet(self, packet, dest):
@@ -45,7 +48,6 @@ class RIPRouter (Entity):
 		return self.routing_table.t
 
 	def discover(self, packet, port):
-		print("Got a DiscoveryPacket")
 		if packet.is_link_up:
 			self.routing_table.init_new_link(packet.src, port)
 		else:
@@ -53,10 +55,15 @@ class RIPRouter (Entity):
 		self.send_updates()
 
 	def update(self, packet, port):
-		print("Got a Routing Update")
+		if self.routing_table.update(packet, port):
+			return
+		self.send_updates()
 
 	def forward(self, packet, port):
-		print("Got a packet to forward")
+		try:
+			self.send_packet(packet, packet.dst)
+		except KeyError:
+			return
 
 class RoutingTable(object):
 	def __init__(self, owner):
@@ -80,9 +87,34 @@ class RoutingTable(object):
 			self.t[dest][port] = None
 		del self.neighbors[neighbor]
 
+	def update(self, packet, port):
+		paths = packet.paths
+		src = packet.src
+		convergence = False
+		for path in paths:
+			if path not in self.destinations:
+				self.add_destination(path)
+		for dest in self.destinations:
+			if dest == src:
+				pass
+			elif dest not in paths:
+				self.t[dest][self.neighbors[src]] = float('inf')
+				convergence = False
+			else:
+				if self.t[dest][self.neighbors[src]] == 1 + paths[dest]:
+					convergence = True 
+				else:
+					self.t[dest][self.neighbors[src]] = 1 + paths[dest]
+					convergence = False
+		for dest in src.routing_table.destinations:
+			if dest not in self.destinations:
+				self.add_destination(dest)
+				convergence = False
+		return convergence
+
 	def add_destination(self, dest):
 		self.t[dest] = [float('inf') for i in range(self.owner.get_port_count())]
-		if dest not  in self.destinations:
+		if dest not in self.destinations:
 			self.destinations.append(dest)
 
 	def add_neighbor(self, neighbor, port):
@@ -109,13 +141,4 @@ class RoutingTable(object):
 			s += "d: " + str(dest) + " -->" + str(self.t[dest]) + ", "
 		s += "]"
 		return s
-			
-class Neighbor(object):
-	def __init__(self, name, port):
-		self.name = name
-		self.port = port
-		self.destinations = {}
-
-	def __repr__(self):
-		return str(self.name) + " --> " + str(self.destinations)
 
